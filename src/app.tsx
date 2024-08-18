8dada6edf4096a841f57394218eeb1e25ac221aa
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-08-18 19:21:55 -0300

    feat: add a form with name and email fields

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,30 @@
+import { FormEvent } from 'react';
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+
 export function App() {
-	return <h1>Hello</h1>;
+	function handleSubmit(event: FormEvent) {
+		event.preventDefault();
+	}
+
+	return (
+		<div className="bg-zinc-100 text-zinc-900 min-h-screen flex justify-center items-center">
+			<form onSubmit={handleSubmit} className="w-96 p-10 border rounded-md bg-zinc-50 space-y-4" >
+				<div className="space-y-2">
+					<Input
+						type="text"
+						name="name"
+						placeholder="Name"
+					/>
+					<Input
+						type="email"
+						name="email"
+						placeholder="E-mail"
+					/>
+				</div>
+
+				<Button type="submit" className="w-full">Save</Button>
+			</form>
+		</div>
+	);
 }
